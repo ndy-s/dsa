7dfdf3800b9b043ee4a72583b92ec72463e90d02
ndys <hendy.sg02@gmail.com>
2024-10-29 23:02:47 +0700

    [feat] Java: add java dsa

---
+++ Java/src/main/java/hendys/algorithms/App.java
@@ -0,0 +1,17 @@
+package hendys.algorithms;
+
+import hendys.algorithms.LinkedList.DoublyLinkedList;
+
+import java.lang.reflect.Array;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println("TEST");
+    }
+}
---
+++ Java/src/main/java/hendys/algorithms/Queue/Queue.java
@@ -0,0 +1,13 @@
+package hendys.algorithms.Queue;
+
+public interface Queue<T> {
+    public void offer(T elem);
+
+    public T poll();
+
+    public T peek();
+
+    public int size();
+
+    public boolean isEmpty();
+}
