02b5af8e889e092f581fb530fb5c764a97761475
ndys <hendy.sg02@gmail.com>
2025-02-17 10:32:31 +0700

    [feat] Binary Search: add binary search (recursive & iterative) implementation

--- Java/src/main/java/hendys/algorithms/searching/Search.java
+++ Java/src/main/java/hendys/algorithms/searching/Search.java
@@ -2,7 +2,7 @@ package hendys.algorithms.searching;
 
 public class Search {
 
-    public int linearSearch(int[] array,  int target) {
+    public int linearSearch(int[] array, int target) {
         for (var i = 0; i < array.length; i++) {
             if (array[i] == target) {
                 return i;
@@ -11,4 +11,48 @@ public class Search {
 
         return -1;
     }
+
+    public int binarySearchRec(int[] array, int target) {
+        return binarySearchRec(array, target, 0, array.length - 1);
+    }
+
+    private int binarySearchRec(int[] array, int target, int left, int right) {
+        if (right < left) {
+            return -1;
+        }
+
+        int middle = (left + right) / 2;
+
+        if (array[middle] == target) {
+            return middle;
+        }
+
+        if (target < array[middle]) {
+            return binarySearchRec(array, target, left, middle - 1);
+        } else {
+            return binarySearchRec(array, target, middle + 1, right);
+        }
+    }
+
+    public int binarySearch(int[] array, int target) {
+        var left = 0;
+        var right = array.length - 1;
+
+        while (left <= right) {
+            var middle = (left + right) / 2;
+
+            if (array[middle] == target) {
+                return middle;
+            }
+
+            if (target < array[middle]) {
+                right = middle - 1;
+            } else {
+                left = middle + 1;
+            }
+        }
+
+        return -1;
+    }
+
 }
--- Java/src/main/java/hendys/algorithms/searching/SearchApp.java
+++ Java/src/main/java/hendys/algorithms/searching/SearchApp.java
@@ -1,11 +1,27 @@
 package hendys.algorithms.searching;
 
+import java.util.Arrays;
+
 public class SearchApp {
     public static void main(String[] args) {
         int[] numbers = { 7, 1, 3, 6, 5 };
         var search = new Search();
-        var index = search.linearSearch(numbers, 3);
 
-        System.out.println(index);
+        // Linear search
+        var linearIndex = search.linearSearch(numbers, 3);
+        System.out.println("Linear Search Index: " + linearIndex);
+
+        int[] numbersCopy = Arrays.copyOf(numbers, numbers.length);
+        Arrays.sort(numbersCopy);
+
+        // Binary search (recursive)
+        var binaryRecIndex = search.binarySearchRec(numbersCopy, 3);
+        System.out.println("Binary Search Recursive Index: " + binaryRecIndex);
+
+        // Binary search (iterative)
+        var binaryIndex = search.binarySearch(numbersCopy, 7);
+        System.out.println("Binary Search Index: " + binaryIndex);
+
+
     }
 }
